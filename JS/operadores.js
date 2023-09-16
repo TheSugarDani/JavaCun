5230eb2d4c811930de01a63afdb21af352999fce
Daniel alejandro caleño <daniel.caleno@cun.edu.co>
2023-09-15 20:40:04 -0500

    my first commit

---
+++ JS/operadores.js
@@ -0,0 +1,21 @@
+//definicon de variables 
+let num1, num2, resultado
+
+//adigancion de valores a variables 
+num1 = 21
+num2 = 23
+
+//suma 
+resultado = num1 + num2
+// imprimir el resultado en la consola del navegador 
+console.log("el resultado de la suma es: " + resultado) 
+
+//resta 
+resultado = num1 - num2
+console.log("el resultado de la resta es: " + resultado)
+
+//multiplicacion
+resultado = num1 * num2
+console.log("el resultado de la multiplicacion es: " +resultado)
+
+
---
+++ JS/variables.js
@@ -0,0 +1,44 @@
+ //console.log("hola mundo")
+
+// ejercicio 1
+var nomnbre // definicion de la variable
+nombre = 'daniel'  // asignacion de valor tipo string
+console.log(nombre) //imprimir variable
+
+// variable tipo number
+var numero 
+numero = 21 
+console.log(numero)
+
+// variable tipo boolean
+var mayordeEdad
+mayordeEdad = true
+console.log(mayordeEdad)
+
+//variable tipo objeto arreglo
+var objetoArreglo
+objetoArreglo = [2,4,6]
+console.log(objetoArreglo)
+
+//variable tipo objeto (JSON)
+var objetoJSON = {
+'Nombre':'Andres',  //estructura clave:valor
+'Edad':23, 
+'estatura':1.73
+}
+console.log(objetoJSON)
+
+//variable tipo funcion 
+var saludo = function(){
+    console.log("buenos dias por la mañana")
+}
+(saludo)
+
+//varibale tipo null
+var dato
+dato = null
+console.log(dato)
+
+//variable indefinida
+var naranja 
+console.log(naranja)
