3dd1647ee38c4a16d29032758a7478e1c99379ff
Daniel alejandro caleño <daniel.caleno@cun.edu.co>
2023-09-17 11:47:02 -0500

    my first commit

--- JS/operadores.js
+++ JS/operadores.js
@@ -5,6 +5,7 @@ let num1, num2, resultado
 num1 = 21
 num2 = 23
 
+/*
 //suma 
 resultado = num1 + num2
 // imprimir el resultado en la consola del navegador 
@@ -19,7 +20,35 @@ resultado = num1 * num2
 console.log("el resultado de la multiplicacion es: " + resultado)
 
 //division 
-resultado = num1 / num2
+resultado = num2 / num1
 console.log("el resultado de la divison es: "+ resultado)
+
+//modulo 
+resultado = num2 % num1 
+console.log("el resultado de la modulacion es: " + resultado)
+
+//exponenciacion 
+resultado = num1 ** num2 
+console.log(" el resultado de la exponenciacion es: " + resultado)
+*/
+
+//funcion alert
+
+let num3, num4
+/*
+num3 = 20
+num4 = 23
+
+resultado = num3 + num4 
+alert(`la suma entre ${num3} y ${num4} es ${resultado}`)
+*/
+
+num3 =prompt ('digite el primer numero')
+num4 =prompt ('digite el segundo numero')
+resultado = num3 + num4 
+
+resultado = parseInt(num3) + parseInt(num4)
+
+alert(`la suma entre ${num3} y ${num4} es ${resultado}`)
 
 
