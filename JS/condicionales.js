d176b618fc6884f88a8cba5f78ae5d02265170da
Daniel alejandro caleño <daniel.caleno@cun.edu.co>
2023-09-22 08:29:36 -0500

    actualizacion

---
+++ JS/condicionales.js
@@ -0,0 +1,29 @@
+/*let edad = 18
+console.log(edad)
+
+if(edad >= 18){
+    console.log('es mayor de edad')
+}else{
+    console.log('es menor de edad')
+
+}*/
+/*let edad =parseInt(prompt('digite su edad'))
+if(edad<=13){
+    alert('eres un niño')
+}
+else if(edad>=14 && edad <=17){
+    alert('eres un adolescente')
+}else if (edad >=18 && edad <=25){
+    alert('eres un joven adulto')
+}else if(edad >=26 && edad <=50){
+    alert('eres un adulto')
+}else if(edad >=51 && edad <=90){
+    alert('eres un viejo')
+}
+*/
+let pais =prompt('digite el pais de nacimiento (colombia, peru, EEUU, inglaterra): ')
+if(pais =='colombia'|| pais =='peru'){
+    alert('tu pais de habla hispana')
+}else if (pais == 'EEUU','eeuu' || pais == 'inglaterra'){
+    alert('tu pais es de habla inglesa')
+}
